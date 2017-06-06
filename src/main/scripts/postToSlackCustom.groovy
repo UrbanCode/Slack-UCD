cbc438731bf5bdd4afa2f7f6561f824c7bedf7c2
Glen Hickman <gchickma@us.ibm.com>
2017-06-06 10:52:27 -0500

    change to use httpbuilder

--- src/main/scripts/postToSlackCustom.groovy
+++ src/main/scripts/postToSlackCustom.groovy
@@ -8,77 +8,65 @@
 import com.urbancode.air.AirPluginTool;
 import groovy.json.JsonBuilder;
 import groovy.json.JsonSlurper;
-import org.apache.commons.httpclient.HttpClient
-import org.apache.commons.httpclient.methods.PostMethod
-import org.apache.commons.httpclient.methods.StringRequestEntity
+
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.ContentType.JSON
+import static groovyx.net.http.Method.POST
 
 final def airTool = new AirPluginTool(args[0], args[1])
 
-/* Here we call getStepProperties() to get a Properties object that contains the step properties
-* provided by the user.
-*/
 final def props = airTool.getStepProperties()
 
-// properties
 final def webhook = props['webhook'];
 final def slackChannels = props['channels'].split(",|\n")*.trim() - "";
 final def slackUsername = props['username'];
 final def emoji = props['emoji'];
 final def slackAttachment = props['attachment'];
 
-slackChannels.eachLine { slackChannel ->
-   slackChannel = URLDecoder.decode(slackChannel, "UTF-8" );
-   if (!slackChannel.startsWith("@") && !slackChannel.startsWith("#")) {
-      throw new RuntimeException("ERROR:: Invalid slack channel format passed: '${slackChannel}'. Must start with either # or @.")
-   }
+slackChannels.each { slackChannel ->
+  slackChannel = URLDecoder.decode(slackChannel, "UTF-8" );
+  println "slackChannel: " + slackChannel
+  if (!slackChannel.startsWith("@") && !slackChannel.startsWith("#")) {
+    throw new RuntimeException("ERROR:: Invalid slack channel format passed: '${slackChannel}'. Must start with either # or @.")
+  }
 }
 
-slackChannels.eachLine { slackChannel ->
-   def slurped = new JsonSlurper().parseText(slackAttachment)
-   def attachmentJson = new JsonBuilder(slurped)
+slackChannels.each { slackChannel ->
+  def slurped = new JsonSlurper().parseText(slackAttachment)
+  def attachmentJson = new JsonBuilder(slurped)
 
-   attachmentJson.content[0].ts = "" + System.currentTimeMillis()/1000;
+  println "attachmentJson.content: " + attachmentJson.content
 
-   // JSON message composition
-   def json = new JsonBuilder();
+  attachmentJson.content[0].ts = "" + System.currentTimeMillis()/1000;
 
-   try {
-
-     json {
+  def json = new JsonBuilder();
+  try {
+    json {
         channel slackChannel
         username slackUsername
         icon_emoji emoji
         attachments attachmentJson.content
-      }
-      println "DEBUG:: JSON Payload"
-      println json.toPrettyString();
-    } catch (Exception exception) {
+    }
+    println "DEBUG:: JSON Payload"
+    println json.toPrettyString();
+  } 
+  catch (Exception exception) {
     println "ERROR:: setting path: ${e.message}"
     System.exit(1)
- }
+  }
 
- // HTTP POST to Slack
- try{
-    def requestEntity = new StringRequestEntity(
-        json.toString(),
-        "application/json",
-    "UTF-8"
-    );
-    def http = new HttpClient();
-    def post = new PostMethod(webhook);
-    post.setRequestEntity(requestEntity);
+  println "DEBUG:: POST to Slack: " + webhook
+  def http = new HTTPBuilder( webhook )
+  http.ignoreSSLIssues()
+  http.request (POST, JSON) { req ->
+    body = json.toPrettyString()
 
-    def status = http.executeMethod(post);
-
-    if (status == 200){
-        println "Success: ${status}";
-    } else {
-        println "Failure: ${status}"
-        System.exit(3);
+    response.success = { resp ->
+        println "SUCCESS: Message posted to Slack successfully: ${resp.statusLine}"
     }
-  } catch (Exception e) {
-     println "ERROR:: Unable to set path: ${e.message}"
-     println "[Possible Solution] Confirm the properties by running the Webhook with its associated JSON body in a REST Client."
-     System.exit(2)
+    response.failure = {  resp ->
+        println "ERROR: Failed to post message to Slack: ${resp.statusLine}"
+        System.exit(1)
+    }
   }
 }
